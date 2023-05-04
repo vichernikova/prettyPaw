ce1dadc2e571329705d88adb96d5bb9dc4357f17
vichernikova <victoria.chernikova@outlook.com>
2023-05-05 01:00:09 +0300

    feat: add media queries

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './index.html';
+import 'swiper/scss';
 import './index.scss';
+
+const videoBG = document.querySelector('.video-bg');
+videoBG.innerHTML = `
+<source src="video/video.webm" type="video/webm">
+<source src="video/video.mp4" type="video/mp4">
+`;
