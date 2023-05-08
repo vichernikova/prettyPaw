84b882ba8f67049131768e518d05e04c3d906f22
vichernikova <victoria.chernikova@outlook.com>
2023-05-08 12:27:36 +0300

    feat: add style to 'career' section and 'career' slider

--- src/index.js
+++ src/index.js
@@ -8,6 +8,31 @@ import { slidersInit } from './modules/sliders.js';
 slidersInit('.about__slider', {
 	pagination: {
 		el: '.about__slider-pagination',
+		clickable: true,
+	}
+});
+
+slidersInit('.career__slider', {
+	pagination: {
+		el: '.career__slider-pagination',
+		clickable: true,
+	},
+	breakpoints: {
+		768: {
+			slidesPerView: 'auto',
+			spaceBetween: 20,
+			pagination: false,
+		},
+		1024: {
+			slidesPerView: 'auto',
+			spaceBetween: 26,
+			pagination: false,
+		},
+		1240: {
+			slidesPerView: 'auto',
+			spaceBetween: 30,
+			pagination: false,
+		}
 	}
 });
 
